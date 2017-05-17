5f0ea7f8d8801241894fadc147f5957da84568b0
Jan Van Braeckel <janvanbraeckel1995@hotmail.com>
2017-05-17 20:11:36 +0200

    Use webpack

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,38 +1,14 @@
-const words = document.querySelectorAll('.words > span');
+import { TxtType } from './TxtType';
 
-const reset = () => lettersAnimation();
-
-const lettersAnimation = () => {
-  [].slice.call(words).forEach((word, i) => {
-    const letters = word.querySelectorAll('i');
-    const timeout = i === 0 ? 0 : [].slice.call(words).slice(0, i).reduce((prevVal, el) => {
-      return prevVal + ((el.querySelectorAll('i').length - 1) * 100);
-    }, 0);
-
-    setTimeout(() => {
-      [].slice.call(words).forEach((w) => {
-        if (w !== word) {
-          w.classList.add('hidden');
-        }
-      });
-      word.classList.remove('hidden');
-      [].slice.call(letters).forEach((letter, j) => {
-        setTimeout(() => {
-          letter.classList.remove('out');
-          if (words[words.length - 1] === word && letters[letters.length - 1] === letter) {
-            setTimeout(function () {
-              reset();
-            }, 1000);
-          }
-          if(letters[letters.length -1] === letter){
-            setTimeout(function() {
-              [].slice.call(letters).forEach((l) => l.classList.add('out'));
-            }, 1000);
-          }
-        }, j * 100);
-      });
-    }, timeout + (i * 1000));
-  });
+window.onload = () => {
+  const elements = document.getElementsByClassName('typewriter');
+  for (let i = 0; i < elements.length; i++) {
+    let toRotate = elements[i].getAttribute('data-type');
+    const period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      toRotate = toRotate.replace(/'/g, '"');
+      const txtType = new TxtType(elements[i], JSON.parse(toRotate), period);
+      txtType.tick();
+    }
+  }
 };
-
-lettersAnimation();
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'app.bundle.css'
+});
+
+
+var config = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    app: './src/scripts/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: ['node_modules'],
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+        })
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [path.resolve('./src'), 'node_modules']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: false
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        tslint: {
+          emitErrors: true,
+          failOnHint: true
+        }
+      }
+    }),
+    extractSass,
+  ],
+  devServer: {
+    contentBase: ['./', path.join(__dirname + '/src/images')],
+    compress: true,
+    port: 8080,
+    disableHostCheck: true,
+    stats: {
+      colors: true,
+    },
+    /**
+     * Will do URL rewriting for us since we use HTML 5 mode
+     */
+    historyApiFallback: {
+      index: '/',
+    },
+    /**
+     * Open the browser when starting the project
+     */
+    open: true,
+  },
+};
+
+module.exports = config;
